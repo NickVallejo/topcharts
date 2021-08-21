e697946ff08d8eac0756fc2a75c55e24c30e1754
NickNorthNet <nick@northnetmedia.com>
2021-08-21 15:25:11 -0400

    changed user moodel file

--- models/user_model.js
+++ models/user_model.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose")
 const db_connect = require('../index')
 
-// const userStoreConnection = mongoose.createConnection(
-//   "mongodb+srv://nicovallejo:weareborg@cluster0-p0vwz.azure.mongodb.net/test?retryWrites=true&w=majority",
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     try {
-//       console.log("Connected to user storage...")
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
-// )
-
 const userSchema = mongoose.Schema({
   email: {
     type: String,
